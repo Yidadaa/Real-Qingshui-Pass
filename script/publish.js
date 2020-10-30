12ae8791305357a475d31fc84768850539ba8b43
Yidadaa <yidadaa@qq.com>
2020-10-31 00:31:05 +0800

    feat(script): add deploy script

---
+++ script/publish.js
@@ -0,0 +1,32 @@
+const Git = require('simple-git')
+const path = require('path')
+const shell = require('shelljs')
+const fs = require('fs')
+
+async function main() {
+  const destPath = path.resolve(__dirname, '../')
+  const distPath = path.resolve(__dirname, '../dist')
+  const nowStr = new Date().toLocaleString()
+
+  const git = Git(destPath)
+
+  git.checkout('gh-pages')
+
+  console.log('[git] deploying')
+  // delete old files and copy new file
+  shell.ls(distPath).forEach(v => {
+    let gitFile = path.resolve(destPath, v)
+    if (fs.existsSync(gitFile)) {
+      shell.rm('-rf', gitFile)
+    }
+    shell.cp('-r', path.resolve(distPath, v), destPath)
+    console.log(`[cp] ${gitFile}`)
+  })
+
+  console.log('[git] deploying...')
+  git.add('.')
+    .commit(`deploy(page): version ${nowStr}`)
+    // .push(() => console.log('[git] delploy done'))
+}
+
+main()
--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 
 // import vue material dependencies
-// import { MdButton, MdProgress, MdToolbar, MdList, MdIcon, MdDialogPrompt } from 'vue-material/dist/components'
 import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.min.css'
 import 'vue-material/dist/theme/default.css'
@@ -17,12 +16,6 @@ import PassRulePage from './components/PassRulePage.vue'
 Vue.config.productionTip = false
 
 // config vue material ui framework
-// Vue.use(MdButton)
-// Vue.use(MdProgress)
-// Vue.use(MdToolbar)
-// Vue.use(MdList)
-// Vue.use(MdIcon)
-// Vue.use(MdDialogPrompt)
 Vue.use(VueMaterial)
 
 // config vue router
@@ -31,7 +24,7 @@ Vue.use(VueRouter)
 // config page router
 const router = new VueRouter({
   mode: 'history',
-  base: __dirname,
+  base: process.env.NODE_ENV === 'production' ? 'Real-Qingshui-Pass' : __dirname,
   routes: [{
       path: '/', component: LoginPage
     }, {
